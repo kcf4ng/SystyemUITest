abccb7a3d5baced857109d9a67d8c7a466ad83f0
KCFang <kuanchung.fang@imedtac.com>
2020-09-18 17:45:22 +0800

    Initial commit

---
+++ app/src/main/java/com/practice/systyemuitest/MainActivity.java
@@ -0,0 +1,24 @@
+package com.practice.systyemuitest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        changeSystemUI();
+    }
+    private void changeSystemUI() {
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+    }
+}
